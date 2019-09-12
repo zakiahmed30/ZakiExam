ebf8e921efac4f7274f161e6dfbdc3e70db9bec9
Zaki Ahmed <zakiahmed@Zakis-MBP.home>
2019-09-11 21:57:04 -0400

    first commit

---
+++ src/design/Employee.java
@@ -0,0 +1,26 @@
+package design;
+
+public interface Employee {
+	
+	/*Employee is an Interface which contains multiple unimplemented methods.Again few methods
+	has been declared in below. you need to brainstorm to add more methods to meet the business requirements.
+	*/
+    //please read the following method and understand the business requirements of these following methods
+    //and then implement these in a concrete class.
+
+    //employeeId() will return employee id.
+    int employeeId(1267201);
+
+    //employeeName() will return employee name
+    String employeeName(Zaki);
+
+    //assignDepartment() will assign employee to departments
+    void assignDepartment(Human Resource);
+
+    //calculate employee salary
+    int calculateSalary(100000);
+
+    //employee benefit
+    void benefitLayout(health);
+
+}
